fd46fff29c0fe41829199127470bbf8a4ee66cf1
harsh <harshguptahb055@gmail.com>
2025-10-13 21:57:44 +0530

    implemented routing functionality using tanstack router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import HomePage from './pages/HomePage'
-import LoginForm from './components/LoginForm'
-import AuthPage from './pages/AuthPage'
+import { Outlet } from '@tanstack/react-router';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <>
-      {/* <HomePage/> */}
-      <AuthPage />
+      <Navbar />
+      <Outlet/>
     </>
   )
 }
