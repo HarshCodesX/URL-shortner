e75cada5f853ce053aa94ba7ad9cdcdc53418a51
harsh <harshguptahb055@gmail.com>
2025-10-20 22:23:26 +0530

    to maintain streak

--- Backend/app.js
+++ Backend/app.js
@@ -16,6 +16,7 @@ app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -30,3 +31,5 @@ app.listen(3000, () => {
     connectDB();
     console.log("server is running on http://localhost:3000");
 });
+
+//implement persistent authentication
