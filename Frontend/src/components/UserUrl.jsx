df3397e19cdf3d5e24eae8b1a35ece62160b7f2c
harsh <harshguptahb055@gmail.com>
2025-11-22 05:31:00 +0530

    finish by sunday

---
+++ Frontend/src/components/UserUrl.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { getUserUrls } from '../api/shortUrl.api';
+
+const UserUrl = () => {
+  return (
+    <div>UserUrl</div>
+  )
+}
+
+export default UserUrl;
