2f19d1650212510db54bcf05c5d733c5ba9d8ab9
harsh <harshguptahb055@gmail.com>
2025-11-30 23:41:55 +0530

    added login button

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,12 @@
 import {Link} from "@tanstack/react-router";
+import { useNavigate } from "@tanstack/react-router";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [clickedOnLogin, setClickedOnLogin] = useState(false);
+  const { isAuthenticated } = useSelector((state) => state.auth);
   return (
     <nav className='bg-white shadow-md'>
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
@@ -11,8 +17,29 @@ const Navbar = () => {
                         URL Shortner
                     </Link>
                 </div>
-                <button onClick={() => {
-                  
+                {isAuthenticated ? (
+                  <button className="
+                px-6 py-3
+                bg-blue-600 text-white 
+                rounded-lg 
+                font-medium 
+                shadow-sm 
+                hover:bg-blue-700 
+                transition-all 
+                duration-200 
+                hover:shadow 
+                cursor-pointer
+              " onClick={() => {
+                
+              }}>Logout</button>
+                ) : (
+                  <button onClick={() => {
+                  navigate({ to: "/auth" });
+                  setClickedOnLogin((prev) => !prev);
+                  if(clickedOnLogin){
+                    navigate({ to: "/" });// Redirect to home if continuing without login
+                    setClickedOnLogin(false);
+                  }
                 }} className="
                 px-6 py-3
                 bg-blue-600 text-white 
@@ -24,7 +51,8 @@ const Navbar = () => {
                 duration-200 
                 hover:shadow 
                 cursor-pointer
-              " >Login</button>
+              " >{clickedOnLogin ? "Continue without login" : "Login"}</button>
+                )}
             </div>
         </div>
     </nav>
