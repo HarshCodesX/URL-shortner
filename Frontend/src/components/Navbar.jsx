f4ce09f31317d18da760e37a1efba5531a665cec
harsh <harshguptahb055@gmail.com>
2025-11-30 21:33:22 +0530

    added styling to the button

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -4,17 +4,31 @@ const Navbar = () => {
   return (
     <nav className='bg-white shadow-md'>
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-            <div className='flex justify-between h-16'>
+            <div className='flex justify-between h-16 items-center'>
                 {/* Left side -App Name */}
                 <div className='flex items-center'>
                     <Link to="/" className='text-xl font-bold text-gray-800'>
                         URL Shortner
                     </Link>
                 </div>
+                <button onClick={() => {
+                  
+                }} className="
+                px-6 py-3
+                bg-blue-600 text-white 
+                rounded-lg 
+                font-medium 
+                shadow-sm 
+                hover:bg-blue-700 
+                transition-all 
+                duration-200 
+                hover:shadow 
+                cursor-pointer
+              " >Login</button>
             </div>
         </div>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
